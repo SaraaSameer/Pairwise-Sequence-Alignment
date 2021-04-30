c74f4888b274b3fc8618da2faa8fe455d36831fb
ahsannaqvii <syedahsan13@hotmail.com>
2021-04-30 05:13:43 +0500

    I have added the traceback

--- Sequence.cpp
+++ Sequence.cpp
@@ -18,10 +18,10 @@ private:
 	int gap_penalty;
 
 public:
-	Matrix(int x_axis, int y_axis)
+	Matrix(int gene1Len, int gene2Len)	
 	{
-		x = y_axis + 1;
-		y = x_axis + 1;
+		x = gene2Len + 1;			//gene2 length
+		y = gene1Len + 1; 	//gene 1 length;
 		mat = new int* [x];
 		for (int i = 0; i < x; i++)
 			mat[i] = new int[y];
@@ -93,13 +93,74 @@ public:
 
 
 	 }
+	 void FinalResStrings(){
+	 	int lenGene2=x-1;		//doing minus 1 because upar length mai +1 kiya wa tha.
+	 	int lenGene1=y-1;	//length of gene 1
+	 	
+	 	int maxLength=lenGene2+lenGene1;
+	 	
+	 	int i=lenGene1;								//backtracking from the last value of matrix
+	 	int j=lenGene2;
+//		cout<<"LENGTH OF GENE 1 : " <<lenGene1;
+
+		int xpos=maxLength;	//maxlength =7
+		int ypos=maxLength;
+		
+		//RULES:
+		//1) IF A MATCH THEN GO DIAGNOL
+		//2) IF A MISMATCH GO TO NEAREST BLOCK VALUE.
+		
+		
+		int gene1Ans[maxLength+1],gene2Ans[maxLength+1];								//Final Answers!
+		
+//		cout<<"WOW"<<
+
+		while( !(i == 0 || j == 0) ){								//Stopping condition; stop the search when it reaches matrix[0][0]
+		
+			if( this->gene1[i-1] == this->gene2[j-1] ){								//If it is match then go diagnol.
+				
+				gene1Ans[xpos--]=(int)this->gene1[i-1];
+				gene2Ans[ypos--]=(int)this->gene2[i-1];
+	
+				i--; 
+				j--;
+			}
+			//IF LETTERS ARE NOT SAME THEN  WE NEED TO FIND THE HIGHEST VALUE OF NEIGHBOURS ( ISKO KARNE KELIYE JO UPAR SETMARIX() MAI MINUS KARA THA WOH PLUS KARDO
+			
+			else if ( mat[i-1][j-1] + mismatch_penalty == mat[i][j]  ){	//Diagnol case.
+			
+				gene1Ans[xpos--] = (int)this->gene1[i - 1];
+           	 	gene2Ans[ypos--] = (int)this->gene2[j - 1];
+            
+				i-- ; j-- ;
+				
+			}
+			else if(mat[i-1][j] + gap_penalty == mat[i][j]){	//FOR LEFT CASE
+			
+				gene1Ans[xpos--]=(int)this->gene1[i-1];
+				
+				gene2Ans[ypos--]=(int)'_';
+				i--;
+			}
+			else if(mat[i][j-1] + gap_penalty == mat[i][j]){	//FOR UP CASE.
+			
+				gene1Ans[xpos--]=(int)'_';
+				
+				gene2Ans[ypos--]=(int)this->gene2[i-1];
+				
+				j--;
+			}
+		}
+		
+		
+	 }
 
 };
 
 int main()
 {
-	string gene1 = "ATCGT";      //Remove case sensitivity
-	string gene2 = "TGGTG";
+	string gene1 = "TCG";      //Remove case sensitivity
+	string gene2 = "ATCG";
 
 	// intialsing penalties of different types
 	int misMatchPenalty = -1;
@@ -110,5 +171,6 @@ int main()
 	dp.setPenalty(misMatchPenalty, gapPenalty);
 	dp.setMatrix();
 	dp.Print_Matrix();
+	dp.FinalResStrings();
 
 }

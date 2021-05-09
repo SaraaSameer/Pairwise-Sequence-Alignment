40ea75bd1aab7bc0a30d8f2857312ba2eec6544e
mustafabawany <mustafabawany204@gmail.com>
2021-05-10 02:33:19 +0500

    few changes in openMP

---
+++ Parallel Programming/OMP.cpp
@@ -0,0 +1,111 @@
+#include<iostream>
+#include<algorithm>
+#include<string.h>
+#include<string>
+#include<omp.h>
+
+using namespace std;
+
+int matchPenalty;
+int misMatchPenalty;   
+int gapPenalty;
+
+struct gene_struct {
+    string gene1;
+    string gene2;
+    int rowSize;
+    int colSize;
+    int **mat;
+};
+
+void Set_Matrix(struct gene_struct shared_block){
+
+	#pragma omp parallel
+	#pragma omp for  
+	for (int i = 0 ; i < shared_block.rowSize ; i++)
+	{
+		//cout << "\nThread: " << omp_get_thread_num();
+		cout << endl; 
+		shared_block.mat[i][0] = i * gapPenalty;
+	}
+	
+	#pragma omp for 
+	for (int i = 0 ; i < shared_block.colSize ; i++)
+	{
+		shared_block.mat[0][i] = i * gapPenalty;
+	}
+
+	// Other matrix values
+
+	for (int i = 1 ; i < shared_block.rowSize ; i++)
+	{
+		#pragma omp for 
+		for (int j = 1; j < shared_block.colSize ; j++)
+		{
+			if (shared_block.gene1[j - 1] == shared_block.gene2[i - 1])    //Similar gene values (A==A ||T==T)
+			{
+				shared_block.mat[i][j] = shared_block.mat[i - 1][j - 1] + matchPenalty;
+			}
+			else
+			{
+				shared_block.mat[i][j] = max({ shared_block.mat[i - 1][j - 1] + misMatchPenalty , shared_block.mat[i - 1][j] + gapPenalty, shared_block.mat[i][j - 1] + gapPenalty });
+			}
+		}
+	}
+}
+void Print_Matrix(struct gene_struct shared_block)
+{
+	cout << endl;
+	#pragma omp for 
+	for (int i = 0; i < shared_block.rowSize; i++)
+	{
+		for (int j = 0; j < shared_block.colSize; j++)
+		{
+			cout << shared_block.mat[i][j] << "\t";
+		}
+		cout << "\n";
+	}
+	
+}
+
+int main(int argc, char *argv[]){
+	
+	string gene1, gene2;
+	struct gene_struct shared_block;
+	
+	cout << "Enter First Gene : ";
+	cin >> gene1;
+	cout << "Enter Second Gene : ";
+	cin >> gene2;
+	cout<<"Enter the value of Match Penalty: ";
+	cin >>  matchPenalty;
+	
+	cout<<"Enter the value of misMatchPenalty Penalty: ";
+	cin >>  misMatchPenalty;
+	
+	cout<<"Enter the value of gapPenalty Penalty: ";
+	cin >>  gapPenalty;
+	
+	shared_block.gene1 = gene1;
+	shared_block.gene2 = gene2;
+	shared_block.rowSize = gene2.length()+1;
+	shared_block.colSize = gene1.length()+1;
+
+	shared_block.mat = new int* [shared_block.rowSize];
+	
+	#pragma omp parallel
+	
+	//printf("Number of Threads: %d\n" , omp_get_thread_num());
+	
+	#pragma omp for 
+	
+	for (int i = 0; i < shared_block.rowSize; i++)
+	{
+		shared_block.mat[i] = new int[shared_block.colSize];   
+	}	
+ 
+	Set_Matrix(shared_block);
+	Print_Matrix(shared_block);
+	return 0;
+}
+
